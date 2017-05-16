43ec09fd58e9699e9873cd521bd44ac346c4c481
musangisilvia <musangisilvia@gmail.com>
2017-05-16 17:35:20 +0300

    add different font to header

--- NewInvites/assets/js/scripts.js
+++ NewInvites/assets/js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $('#newInvite').click(function() {
-        window.open('../Template/template.html');
+        window.open('../Template/details.html');
     });
 });

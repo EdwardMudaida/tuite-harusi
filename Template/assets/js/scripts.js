e316ff91650af7189d2a68cddc7f2682f5a01164
musangisilvia <musangisilvia@gmail.com>
2017-05-17 21:57:27 +0300

    Add template

--- Template/assets/js/scripts.js
+++ Template/assets/js/scripts.js
@@ -5,15 +5,15 @@ $(document).ready(function() {
             $("#confirm").show();
             $("#confirm").fadeOut(5000);
             // declare an array that will store the user input
-            var details = [];
-            var bride = document.getElementById("brideName").value;
-            var groom = document.getElementById("groomName").value;
-            var date = document.getElementById("Date").value;
-            var time = document.getElementById("Time").value;
-            var venue = document.getElementById("Venue").value;
-            var message = document.getElementById("message").value;
-            details.push(bride, groom, date, time, venue, message);
-            window.open('../Template/template.html');
+            var bride = $("#brideName").val();
+            var groom = $("#groomName").val();
+            var date = $("#Date").val();
+            var time = $("#Time").val();
+            var venue = $("#Venue").val();
+            var message = $("#message").val();
+            window.open("../Template/template.html");
+            $("#bride").append('<span class="bride" contenteditable="true">' + bride + '</span>');
+            $("#groom").append(groom);
         });
     });
 });

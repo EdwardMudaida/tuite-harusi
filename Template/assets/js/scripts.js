046e74b0d5dad94c0b312a928c3acb3e8570ea07
musangisilvia <musangisilvia@gmail.com>
2017-05-17 17:34:40 +0300

    the button opens the template

--- Template/assets/js/scripts.js
+++ Template/assets/js/scripts.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
-	$("#coupleInfo").submit(function(event) {
-		event.preventDefault();
-		$("#submitInfo").click(function() {
-			$("#confirm").show();
-			$("#confirm").fadeOut(5000);
-		});
-	});
+    $("#coupleInfo").submit(function(event) {
+        event.preventDefault();
+        $("#submitInfo").click(function() {
+            $("#confirm").show();
+            $("#confirm").fadeOut(5000);
+            // declare an array that will store the user input
+            var details = [];
+            var bride = document.getElementById("brideName").value;
+            var groom = document.getElementById("groomName").value;
+            var date = document.getElementById("Date").value;
+            var time = document.getElementById("Time").value;
+            var venue = document.getElementById("Venue").value;
+            var message = document.getElementById("message").value;
+            details.push(bride, groom, date, time, venue, message);
+            window.open('../Template/template.html');
+        });
+    });
 });

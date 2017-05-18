eea84038415f93ef9dc96ae17accd28d75647610
musangisilvia <musangisilvia@gmail.com>
2017-05-18 14:51:54 +0300

    landing page linked to form

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,15 @@
-$(window).scroll(function() {
-  $(".slideanim").each(function(){
-    var pos = $(this).offset().top;
+$(document).ready(function() {
+    $(window).scroll(function() {
+        $(".slideanim").each(function() {
+            var pos = $(this).offset().top;
 
-    var winTop = $(window).scrollTop();
-    if (pos < winTop + 600) {
-      $(this).addClass("slide");
-    }
-  });
+            var winTop = $(window).scrollTop();
+            if (pos < winTop + 600) {
+                $(this).addClass("slide");
+            }
+        });
+    });
+    $(".modals h2").click(function() {
+        window.open("./Template/details.html", "_self")
+    })
 });
